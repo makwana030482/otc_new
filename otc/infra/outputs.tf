7eedd7b3a4f858180d48a6e2739f223370ea2b22
Nitin Makwana <nitin.makwana@t-systems.com>
2023-10-18 09:21:35 +0000

    deleted directories and added git ignore file

--- otc/infra/outputs.tf
+++
@@ -1,7 +0,0 @@
-#output "public_subnet01" {
-#  value = opentelekomcloud_vpc_subnet_v1.public_subnet01
-#}
-
-output "vpc" {
-  value = module.vpc.public_subnet01
-}
--- otc/modules/jumphost/output.tf
+++
@@ -1,8 +0,0 @@
-
-output "jumphost_address" {
-  value = opentelekomcloud_vpc_eip_v1.jumphost_eip.publicip[0].ip_address
-}
-
-output "jumphost_private_address" {
-  value = opentelekomcloud_compute_instance_v2.jump_host.access_ip_v4
-}
